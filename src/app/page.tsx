70b284b4bc58623ec76ed4c4f73a09d0e3f6fd76
vxtxcx <victorvicencio932@gmail.com>
2025-07-02 22:56:34 -0400

    estructura pagina
    se creo la estructura de la pagina, header, main, footer y se le agrego algunos estilos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 
+import styles from './page.module.css'
+import './globals.css';
+
 export default function Home() {
   return (
-    <div>
-      <h1> comienzo evaluacion numero 3 </h1>
+    <div className="grid-container">
+      <header className="header">
+        <h1 className={styles.title}>HEADER</h1>
+      </header>
+      <main className="main">
+        <h1 className={styles.title}>MAIN</h1>
+      </main>
+      <footer className="footer">
+        <h1 className={styles.title}>FOOTER</h1>
+      </footer>
     </div>
   );
 }
