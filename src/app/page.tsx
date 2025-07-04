6062b4e507e7f78f2331dbf56c7498478ab4ab59
vxtxcx <victorvicencio932@gmail.com>
2025-07-04 17:46:30 -0400

    PROTOTIPO PRINCIPAL
    se creo la estructura de la web, se introdujo un txt con la idea y el desarrollo del trabajo, y tambien se estilizo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,28 @@ export default function Home() {
   return (
     <div className="grid-container">
       <header className="header">
-        <h1 className={styles.title}>HEADER</h1>
+        <div className="headerBox">
+          <h1 className={styles.headerFont}>GESTOR DE EVENTOS</h1>
+          <nav className="listaBotones">
+            <ul><button>REGISTRAR</button></ul>
+            <ul><button>MOSTRAR</button></ul>
+            <ul><button>EDITAR</button></ul>
+            <ul><button>ELIMINAR</button></ul>
+          </nav>
+        </div>
       </header>
       <main className="main">
-        <h1 className={styles.title}>MAIN</h1>
+        <h1 className={styles.tituloPresentacionFont}>CONVIÉRTETE <br />EN WEB DEVELOPER </h1> <br />
+        <h3 className={styles.subtituloPresentacionFont}>Ofrecemos Bootcamp intensivo en Front-End, Back-End y Full Stack <br /> alrededor de todo Chile para impulsar tu carrera digital.</h3> <br />
+        <div className='eventosARealizar'>
+          <h3 className={styles.subtituloEventosARealizar}>EVENTOS A REALIZAR:</h3>
+          <div className='mostrarEventos'>
+
+          </div>
+        </div>
       </main>
       <footer className="footer">
-        <h1 className={styles.title}>FOOTER</h1>
+        <h1 className={styles.headerFont}>PROXIMAMENTE A REALIZAR MUCHOS MAS EVENTOS!</h1>
       </footer>
     </div>
   );
