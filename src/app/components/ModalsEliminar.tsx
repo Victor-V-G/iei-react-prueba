dea5c9f5a4c4859fe4f91aef09d23a175f8c36aa
vxtxcx <victorvicencio932@gmail.com>
2025-07-05 22:14:50 -0400

    ACTUALIZACION MODALS ELIMINAR
    ahora si no hay nada en localStorage, el modal Eliminar muestra que no hay nada para eliminar, pero si hay algo en LocalStorage muestra los datos, todo mediante un useEffect

--- src/app/components/ModalsEliminar.tsx
+++ src/app/components/ModalsEliminar.tsx
@@ -1,22 +1,56 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import { InterfaceModalsProps } from '../interfaces/InterfaceModalsProps'
 import ComponenteRecuperarDatosEliminar from './ComponenteRecuperarDatosEliminar'
 import './modals.styles.css'
 
 const ModalsEliminar = ({isOpen, closeModal}: InterfaceModalsProps) => {
-    if (!isOpen) {
-      return null
-    } 
+     const [hayDatos, setHayDatos] = useState(false)
 
-  return (
-    <div className="ModalsBox">
-      <div className="ModalsContent">
-        <h1>MODALS ELIMINAR</h1>
-        <ComponenteRecuperarDatosEliminar traerDatos={() => {}}/>
-        <button onClick={closeModal}>Cerrar</button>
+  useEffect(() => {
+    if (!isOpen) return
+
+    const miStorage = window.localStorage
+    let ListaStr = miStorage.getItem("AlmacenarDatosEventos")
+    let ListaParse = []
+
+    if (ListaStr != null) {
+      ListaParse = JSON.parse(ListaStr)
+    } else {
+      ListaParse = []
+    }
+    if (ListaParse.length > 0) {
+      setHayDatos(true)
+    } else {
+      setHayDatos(false)
+    }
+  }, [isOpen])
+
+  if (!isOpen){
+    return null
+  }
+
+  if (hayDatos) {
+    return (
+      <div className="ModalsBox">
+        <div className="ModalsContent">
+          <h1>MODALS ELIMINAR</h1>
+          <ComponenteRecuperarDatosEliminar traerDatos={() => {}}  />
+          <button onClick={closeModal}>Cerrar</button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className="ModalsBox">
+        <div className="ModalsContent">
+          <h1>MODALS ELIMINAR</h1>
+          <p>No hay datos para eliminar</p>
+          <button onClick={closeModal}>Cerrar</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default ModalsEliminar
