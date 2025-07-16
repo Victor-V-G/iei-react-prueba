449d0ec21e51e96dc619285c5de72ca5a4e17ad1
vxtxcx <victorvicencio932@gmail.com>
2025-07-16 13:20:23 -0400

    firebase datos agregados
    se agrego las dependencias de firebase del proyecto creado

---
+++ src/app/firebase/Credenciales.tsx
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD1kDto1MsiV3pf_T7Lxz26d3DivzGCX8I",
+  authDomain: "iei-firebase-eva4-react.firebaseapp.com",
+  projectId: "iei-firebase-eva4-react",
+  storageBucket: "iei-firebase-eva4-react.firebasestorage.app",
+  messagingSenderId: "772345680235",
+  appId: "1:772345680235:web:0d56ed58e77950453b3c2e",
+  measurementId: "G-6ZBLP9VX6X"
+};
